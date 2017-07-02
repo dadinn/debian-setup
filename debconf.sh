5b3d6ecb403fc2d0f196c4408cddaa6fba0fd107
Daniel Dinnyes <ddinnyes@gmail.com>
2017-07-02 18:39:31 +0100

    rewriten debconf script
    - be more portable using only posix shell features

--- debconf.sh
+++ debconf.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-STATEFILE=.debian-state
-RELEASE=jessie
-
-function apt-init {
-    cat >> /etc/apt/apt.conf <<EOF
-APT::Get::Install-Recommends "false";
-APT::Get::Install-Suggests "false";
-EOF
-    echo "APT_INIT=1" >> $STATEFILE
-}
+#!/bin/sh
 
 function sources-backports {
     if [ ! -f /etc/apt/sources.list.d/backports.list ]
@@ -111,6 +100,61 @@ function install-samba {
     esac
 }
 
+
+init_apt () {
+    cat >> /etc/apt/apt.conf.d/norecommends <<EOF
+APT::Get::Install-Recommends "false";
+APT::Get::Install-Suggests "false";
+EOF
+}
+
+configure_timezone () {
+    dpkg-reconfigure tzdata
+}
+
+init_sudouser () {
+    if [ $# -eq 1 ]
+    then
+	SUDOUSER=$1
+    else
+	echo "called init_sudouser with $# args: $@" >&2
+	exit1
+    fi
+    useradd -m -G sudo $SUDOUSER
+    passwd $SUDOUSER
+    passwd -l root
+}
+
+install_zfs () {
+    RELEASE=$(cat /etc/debian_version | sed -e 's;^\([0-9][0-9]*\)\..*$;\1;')
+    case $RELEASE in
+	"8")
+	    echo /etc/apt/sources.list | grep -E '^deb .* jessie main$' | sed -e 's/jessie main/jessie-backports main contrib/' > /etc/apt/sourced.list.d/backports.list
+	    apt update
+	    apt install -y -t jessie-backports zfs-dkms zfs-initramfs
+	    modprobe zfs
+	    ;;
+	"9")
+	    sed -ire 's/^deb (.+) stretch main$/deb \1 stretch main contrib/' /etc/apt/sources.list
+	    apt update
+	    apt install -y zfs-dkms zfs-initramfs
+	    modprobe zfs
+	    ;;
+	*)
+	    echo "ERROR: Debian version $RELEASE is not supported!"
+	    exit 1
+	    ;;
+    esac
+}
+
+install_grub () {
+    [ $# -eq 1 ] && ROOTDEV="$1"
+    [ ! -b $ROOTDEV ] && echo "Not a block device: $ROOTDEV" && exit 1 >&2
+    apt install -y grub-pc
+    grub-install $ROOTDEV
+    update-grub
+}
+
 function install-zfs {
     read -p "Install ZFS tools & kernel modules? [y/N]" zfs
     case $zfs in
@@ -184,83 +228,100 @@ function install-extra {
     esac
 }
 
-function usage {
+ARCH=${ARCH:-amd64}
+LOCALE=${LOCALE:-en_US.UTF-8}
+ZPOOL=""
+
+usage () {
     cat <<EOF
-Initialize Debian system.
+
+Configure a fresh Debian system installation.
 
 USAGE:
 
-$0 [-N]
+$0 [OPTIONS]
 
-With option -N, ask to redo previously ignored steps too.
+Valid options are:
+
+-a ARCH
+Choose architecture of the new installation (default $ARCH)
+
+-l LOCALE
+
+Set system locale to use (default $LOCALE)
+
+-s USER
+
+Name for sudo user instead of root
+
+-z POOL
+Set name for ZFS pool to be used
+
 EOF
 }
 
-LEVEL=1
+if [ $(id -u) -ne 0 ]
+then
+    exit 1
+fi
 
-while getopts "N" opt
+while getopts 'a:l:n:s:z:h' opt
 do
     case $opt in
-	N)
-	    LEVEL=3
+	a)
+	    ARCH=$OPTARG
 	    ;;
-	?)
-	    echo "INVALID ARGUMENT: $OPTARG" >&2
-	    exit -1
+	l)
+	    LOCALE=$OPTARG
+	    ;;
+	n)
+	    HOSTNAME=$OPTARG
+	    ;;
+	s)
+	    SUDOUSER=$OPTARG
+	    ;;
+	z)
+	    ZPOOL=$OPTARG
+	    ;;
+	h)
+	    usage
+	    exit 0
+	    ;;
+	:)
+	    echo "ERROR: Missing argument for potion: -$OPTARG" >&2
+	    exit 1
+	    ;;
+	\?)
+	    echo "ERROR: Illegal option -$OPTARG" >&2
+	    exit 1
 	    ;;
 	*)
 	    usage
-	    exit -1
+	    exit 1
 	    ;;
     esac
 done
 
-if [ -f $STATEFILE ]
+shift $(($OPTIND - 1))
+
+if [ -z "$SUDOUSER" ]
 then
-    source $STATEFILE
-else
-    echo '# Variable flags for debian-init state' > $STATEFILE
+    echo "ERROR: sudo user must be specified!" >&2
+    exit 1
 fi
 
-if [[ $APT_INIT -lt $LEVEL ]]
-then
-    apt-init
-fi
+init_apt
+apt update
+apt full-upgrade -y
+apt install -y locales
+locale-gen $LOCALE
+configure_timezone
+apt install -y console-setup
+apt install -y lsb-release gdisk cryptsetup sudo
 
-if [[ $SYSTEM_UPGRADE -lt $LEVEL ]]
-then
-    system-upgrade
-fi
+init_sudouser $SUDOUSER
 
-if [[ $INSTALL_GRSEC -lt $LEVEL ]]
-then
-    install-grsec
-fi
+if [ ! -z "$ZPOOL" ] && install_zfs
+install_grub
 
-if [[ $INSTALL_ZFS -lt $LEVEL ]]
-then
-    install-zfs
-fi
-
-if [[ $INSTALL_SAMBA -lt $LEVEL ]]
-then
-    install-samba
-fi
-
-if [[ $INSTALL_KVM -lt $LEVEL ]]
-then
-    install-kvm
-fi
-
-if [[ $INSTALL_DOCKER -lt $LEVEL ]]
-then
-    install-docker
-fi
-
-if [[ $INSTALL_EXTRA -lt $LEVEL ]]
-then
-    install-extra
-fi
-
-system-cleanup
-system-reboot
+echo "Finished configuring Debian system!"
