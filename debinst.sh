c208b0575091649f59cd79ce6d6ab0528d6c0f9a
R Vadai <rvadai@segence.com>
2019-07-27 10:56:58 +0100

    Using spaces instead of tabs

--- debinst.sh
+++ debinst.sh
@@ -9,11 +9,11 @@ ERROR_EXIT() {
     local CODE="$2"
     elif [ "$#" -eq 1 ]
     then
-	local MESSAGE="$1"
-	local CODE=1
+    local MESSAGE="$1"
+    local CODE=1
     else
-	echo "ERROR: calling ERROR_EXIT incorrectly!" >&2
-	exit 1
+    echo "ERROR: calling ERROR_EXIT incorrectly!" >&2
+    exit 1
     fi
 
     echo "ERROR: $MESSAGE" >&2
@@ -23,17 +23,17 @@ ERROR_EXIT() {
 bootstrap () {
     if [ $# -eq 4 ]
     then
-	local TARGET=$1
-	local ARCH=$2
-	local RELEASE=$3
-	local MIRROR=$4
+    local TARGET=$1
+    local ARCH=$2
+    local RELEASE=$3
+    local MIRROR=$4
     else
-	ERROR_EXIT "calling bootstrap with $# args: $@"
+    ERROR_EXIT "calling bootstrap with $# args: $@"
     fi
 
     if ! type debootstrap 2>&1 > /dev/null
     then
-	apt install -y debootstrap
+    apt install -y debootstrap
     fi
 
     echo "Bootstrapping Debian release $RELEASE archictecture $ARCH..."
@@ -85,53 +85,56 @@ EOF
 while getopts 'a:r:m:t:Xh' opt
 do
     case $opt in
-	a)
-	    case $OPTARG in
-		amd64|arm64|armel|armhf|i368|mips|mips64el|mipsel|powerpc|ppc64el|s390x)
-		    ARCH=$OPTARG
-		    ;;
-		*)
-		    ERROR_EXIT "invalid architecture $OPTARG"
-		    ;;
-	    esac
-	    ;;
-	r)
-	    RELEASE=$OPTARG
-	    ;;
-	m)
-	    MIRROR=$OPTARG
-	    ;;
-	t)
-	    TARGET=$OPTARG
-	    ;;
-	X)
-	    EXECUTE_ONLY=1
-	    ;;
-	h)
+    a)
+        case $OPTARG in
+        amd64|arm64|armel|armhf|i368|mips|mips64el|mipsel|powerpc|ppc64el|s390x)
+            ARCH=$OPTARG
+            ;;
+        *)
+            ERROR_EXIT "invalid architecture $OPTARG"
+            ;;
+        esac
+        ;;
+    r)
+        RELEASE=$OPTARG
+        ;;
+    m)
+        MIRROR=$OPTARG
+        ;;
+    t)
+        TARGET=$OPTARG
+        ;;
+    X)
+        EXECUTE_ONLY=1
+        ;;
+    h)
             usage
             exit 0
-	    ;;
-	:)
-	    exit 1
-	    ;;
-	\?)
-	    exit 1
-	    ;;
+        ;;
+    :)
+        exit 1
+        ;;
+    \?)
+        exit 1
+        ;;
     esac
 done
 
 if [ "$opt" = "?" ]; then
-	echo "Running with default values:"
-	echo "    Release: ${RELEASE}"
-	echo "    Arch: ${ARCH}"
-	echo "    Mirror: ${MIRROR}"
-	echo "    Installation target: ${TARGET}"
+    echo "Running with default values:"
+    echo "    Release: ${RELEASE}"
+    echo "    Arch: ${ARCH}"
+    echo "    Mirror: ${MIRROR}"
+    echo "    Installation target: ${TARGET}"
+    echo ""
+    echo "To display usage information, run '${0} -h'"
+    echo ""
     read -p 'No arguments set, are you sure you want to proceed with default values? [y/N]: ' PROCEED_WITH_DEFAULTS
 
-	if [ "$PROCEED_WITH_DEFAULTS" != "y" ]; then
-		echo "Installation aborted"
+    if [ "$PROCEED_WITH_DEFAULTS" != "y" ]; then
+        echo "Installation aborted"
         exit 0
-	fi
+    fi
 fi
 
 shift $(($OPTIND - 1))
